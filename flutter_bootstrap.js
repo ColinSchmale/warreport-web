711869d1404cac3f812559adb6e513bc4142d6a6
ColinSchmale <cs@nuuk.de>
2025-08-03 17:32:15 +0200

    2.33.13 Update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -34,11 +34,11 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"109150893958777c8f2215f6cfd3e89e984e8dea","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a5767","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3243329592"
+    serviceWorkerVersion: "1156790233"
   }
 });
