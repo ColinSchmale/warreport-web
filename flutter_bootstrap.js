a573896e56fe84ecfd049d4382000dd8e6f7324b
ColinSchmale <mail@colinschmale.de>
2025-10-21 23:27:54 +0200

    3.0.1 Update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4051132640"
+    serviceWorkerVersion: "2357428290"
   }
 });
