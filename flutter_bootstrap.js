7b42e47b82184b21c863f0b265c3425d224069e1
ColinSchmale <mail@colinschmale.de>
2025-11-01 01:48:23 +0100

    Fix Auth with Discord

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2822746800"
+    serviceWorkerVersion: "4286671424"
   }
 });
