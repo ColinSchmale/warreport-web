e1933f4cc362bd5f7a4742bdd466a430aa1bb477
Colin Schmale <mail@colinschmale.de>
2026-04-13 22:39:55 +0200

    Replace image proxy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2780275847" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "303724137" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
