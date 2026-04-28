266c9bdbb3587766e253d47afabafb2569704ddc
Colin Schmale <mail@colinschmale.de>
2026-04-28 15:24:50 +0200

    3.7.10 Update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "807657865" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2534362810" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
